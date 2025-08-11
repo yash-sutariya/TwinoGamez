d5173a6586e688dc516e6f24a2652b6ffa82a523
Yash Sutariya <84495766+yash-sutariya@users.noreply.github.com>
2025-08-11 16:36:11 +0530

    Update page.tsx

--- src/app/game-page/page.tsx
+++ src/app/game-page/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import Script from "next/script";
+import { Suspense } from 'react';
 import GamePage from "./game-page";
 
 export const metadata: Metadata = {
@@ -11,25 +11,9 @@ export const metadata: Metadata = {
 export default function TwinoGamez() {
   return (
     <>
-      <Script
-        id="facebook-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '793714741985467');
-            fbq('track', 'PageView');
-          `,
-        }}
-      />
-      <GamePage />
+      <Suspense fallback={<div>Loading...</div>}>
+        <GamePage />
+      </Suspense>
     </>
   );
 }
